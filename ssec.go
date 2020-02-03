0349fa9159be34db1770e628350bb77a798338b5
Stuart Hunter <stuart.hunter@comcast.net>
2020-02-03 14:55:32 -0500

    addition of server event stream client ingest

---
+++ ssec.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+//SSE name constants
+const (
+	idTag    = "id"
+	eventTag = "event"
+	dataTag  = "data"
+)
+
+type (
+
+	// SSEvent represents a Server-Sent Event
+	SSEvent struct {
+		URI  string
+		Type string
+		Name string
+		ID   string
+		Data io.Reader
+	}
+
+	// Channel data - visualization
+	Channel struct {
+		Name        string `json:"name"`
+		Accumulated int32  `json:"accumulated,omitempty"`
+		Scaled      int32  `json:"scaled,omitempty"`
+	}
+	// Meter implementation, VU, Spectrum, RMS etc ...
+	Meter struct {
+		Type     string    `json:"type,omitempty"`
+		Channels []Channel `json:"channel,omitempty"`
+	}
+)
+
+var (
+	//ErrNilChan will be returned by Notify if it is passed a nil channel
+	ErrNilChan = fmt.Errorf("nil channel given")
+)
+
+// sseClient is the default client used for requests.
+var sseClient = &http.Client{}
+
+func hasPrefix(s []byte, prefix string) bool {
+	return bytes.HasPrefix(s, []byte(prefix))
+}
+
+func liveReq(verb, uri string, body io.Reader) (*http.Request, error) {
+	req, err := getReq(verb, uri, body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Accept", "text/event-stream")
+
+	return req, nil
+}
+
+var getReq = func(verb, uri string, body io.Reader) (*http.Request, error) {
+	return http.NewRequest(verb, uri, body)
+}
+
+func ssenotify(uri string, evCh chan<- *SSEvent) error {
+
+	if evCh == nil {
+		fmt.Println(`ssenotify`, ErrNilChan)
+		return ErrNilChan
+	}
+
+	req, err := liveReq("GET", uri, nil)
+	if err != nil {
+		ef := fmt.Errorf("error getting sse request: %v", err)
+		fmt.Printf("%v", ef)
+		return ef
+	}
+
+	res, err := sseClient.Do(req)
+	if err != nil {
+		ef := fmt.Errorf("error performing request for %s: %v", uri, err)
+		fmt.Printf("%v", ef)
+		return ef
+	}
+
+	br := bufio.NewReader(res.Body)
+	defer res.Body.Close()
+
+	delim := []byte{':', ' '}
+
+	var thisEvent *SSEvent
+
+	for {
+		bs, err := br.ReadBytes('\n')
+
+		if err != nil && err != io.EOF {
+			return err
+		}
+
+		if len(bs) < 2 {
+			continue
+		}
+
+		spl := bytes.Split(bs, delim)
+
+		if len(spl) < 2 {
+			continue
+		}
+
+		thisEvent = &SSEvent{URI: uri}
+		switch string(spl[0]) {
+		case eventTag:
+			thisEvent.Type = string(bytes.TrimSpace(spl[1]))
+		case dataTag:
+			thisEvent.Data = bytes.NewBuffer(bytes.TrimSpace(spl[1]))
+			evCh <- thisEvent
+		}
+		if err == io.EOF {
+			break
+		}
+	}
+
+	return nil
+}
